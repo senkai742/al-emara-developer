515892924e007a112e071c6e88fea47f6d757cd0
Senkai742 <sakifshahedur7@gmail.com>
2026-06-21 20:41:24 +0600

    fix email not sending

--- app/Mail/PasswordResetMail.php
+++ app/Mail/PasswordResetMail.php
@@ -2,16 +2,14 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class PasswordResetMail extends Mailable implements ShouldQueue
+class PasswordResetMail extends Mailable
 {
-    use Queueable, SerializesModels;
+    use SerializesModels;
 
     public string $token;
     public string $email;
